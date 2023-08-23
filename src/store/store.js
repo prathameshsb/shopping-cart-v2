2ba7715cafe1b0f77303d7fc0c1763fad12aec7d
Prathamesh Borgharkar <prathameshborgharkar@Prathameshs-MacBook-Pro.local>
2023-08-22 22:15:33 -0700

    git repo number 39

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,25 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [
-  process.env.NODE_ENV === 'development' && logger,
-  thunk,
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -17,14 +29,6 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
@@ -32,5 +36,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
